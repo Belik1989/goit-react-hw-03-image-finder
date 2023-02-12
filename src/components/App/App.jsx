b88bac8409dd03a7d54d7ad98d59708766caedac
Bilogolovyi Sergii <sergey.hotchili@gmail.com>
2023-02-12 20:17:14 +0800

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,25 +8,26 @@ import { Button } from '../Button/Button';
 import { Loader } from 'components/Loader/Loader';
 import { AppBox } from './App.styled';
 
-let page = 1;
-
 export class App extends React.Component {
   state = {
     items: [],
     searchValue: '',
     status: `idle`,
     totalHits: 0,
+    page: 1,
   };
 
   submitHandler = async searchValue => {
-    page = 1;
     if (searchValue.trim() === '') {
       alert("You can't search by empty field, try again.");
       return;
     } else {
       try {
         this.setState({ status: 'pending' });
-        const { totalHits, hits } = await fetchImages(searchValue, page);
+        const { totalHits, hits } = await fetchImages(
+          searchValue,
+          this.state.page
+        );
         if (hits.length < 1) {
           this.setState({ status: 'idle' });
           alert(
@@ -49,7 +50,10 @@ export class App extends React.Component {
   onNextPage = async () => {
     this.setState({ status: 'pending' });
     try {
-      const { hits } = await fetchImages(this.state.searchValue, (page += 1));
+      const { hits } = await fetchImages(
+        this.state.searchValue,
+        (this.state.page += 1)
+      );
       this.setState(prevState => ({
         items: [...prevState.items, ...hits],
         status: 'resolved',
@@ -60,42 +64,18 @@ export class App extends React.Component {
   };
 
   render() {
-    const { totalHits, status, items } = this.state;
-    if (status === 'idle') {
-      return (
-        <AppBox>
-          <Searchbar onSubmit={this.submitHandler} />
-        </AppBox>
-      );
-    }
-    if (status === 'pending') {
-      return (
-        <AppBox>
-          <Searchbar onSubmit={this.submitHandler} />
-          <ImageGallery page={page} items={items} />
-          <Loader />
-          {totalHits > 12 && <Button onClick={this.onNextPage} />}
-        </AppBox>
-      );
-    }
-    if (status === 'rejected') {
-      return (
-        <AppBox>
-          <Searchbar onSubmit={this.submitHandler} />
-          <span>Oops!Somthing wrong!Try later.</span>
-        </AppBox>
-      );
-    }
-    if (status === 'resolved') {
-      return (
-        <AppBox>
-          <Searchbar onSubmit={this.submitHandler} />
-          <ImageGallery page={page} items={items} />
-          {totalHits > 12 && totalHits > items.length && (
-            <Button onClick={this.onNextPage} />
-          )}
-        </AppBox>
-      );
-    }
+    const { status, items, page, totalHits } = this.state;
+    const totalPages = items.length / (page * totalHits);
+    return (
+      <AppBox>
+        <Searchbar onSubmit={this.submitHandler} />
+        <ImageGallery page={this.state.page} items={items} />
+        {status === 'pending' && <Loader />}
+        {status === 'rejected' && <span>Oops!Something wrong!Try later.</span>}
+        {totalPages < 1 && status === 'resolved' && (
+          <Button onClick={this.onNextPage} />
+        )}
+      </AppBox>
+    );
   }
 }
